c7073845bcc3810a064f8633f25fefb37fd97569
allon <allon.lu@comm100.com>
2022-06-14 18:04:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Introduction
+
+The project is the back-end program of Vincall's Admin Panel, using .NET core 3.1 technology, providing Vincall's agent, report, voice, and other standard API interfaces.
+
+# Initializing
+Clone VincallService repository on your computer, install dependencies using:
+```
+git clone xxx
+```
+Then if you want to run the project, go to the directory VincallService.
+```
+dotnet run 
+```
+
+The configuration items of the project are as follows and can be modified according to the actual situation.
+```json
+{
+  "ConnectionStrings": {
+    "Vincall": "Server=192.168.2.215;User=sa;Password=Aa00000000;Database=vincall"
+  },
+  "InitDB": "false",
+  "Logging": {
+    "LogLevel": {
+      "Default": "Information",
+      "Microsoft": "Warning",
+      "Microsoft.Hosting.Lifetime": "Information"
+    }
+  },   
+  "OauthUri": "https://oauth.vincall.net",  
+  "TokenPrivateKey": "MIIFHDBOBgkqhkiG9w0***=="
+
+}
+```
+- ConnectionStrings: The connection string to Microsoft SQL server database.
+- InitDB: Whether to recreate the database table structure and initialize the population of data. If the data table structure has already been initialized, it will not be modified again.
+- Logging: The definition and type of the log output are changed
+- OauthUri: Vincall's OAuth 2 service address.
+- TokenPrivateKey: Private key ciphertext required for token issuance.
+
+If you are initializing for the first time, you will need to prepare the MS SQL Server environment. Once you have the database environment ready, you can change the ConnectionString Authdb key and InitDB key.
+
+```
+ "ConnectionStrings": {
+    "AuthDB": "Server=[your ip];User=[your username];Password=[your passwor];Database=vincall"
+  },
+  "InitDB": "true",
+```
+Then to run the program.
+
+```
+dotnet run
+```
+Finally, Let's check database when opening Microsoft SQL server management studio.
+
+That is ok.
---
+++ vincall.service/vincall.service/Controllers/VoiceController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Twilio.TwiML;
+using Twilio.TwiML.Voice;
+using System.Text.RegularExpressions;
+using Vincall.Service.Models;
+using Vincall.Service.Cache;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Vincall.Application;
+using Vincall.Infrastructure;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace vincall.service.Controllers
+{
+    public class VoiceController : Controller
+    {
+
+        private readonly ITwilioSettingCacheService _twilioSettingCacheService;
+        private readonly ICrudServices _services;
+
+        private ILogger<VoiceController> _logger;
+
+        public VoiceController(ITwilioSettingCacheService twilioSettingCacheService, ICrudServices services, ILogger<VoiceController> logger)
+        {
+            _twilioSettingCacheService=twilioSettingCacheService;
+            _services = services;
+            _logger = logger;
+        }
+
+        // POST: /voice
+
+        //[Authorize]
+        [HttpPost("voice")]
+        public async Task<IActionResult> Index(string to, string callingDeviceIdentity)
+        {
+            var twilioSetting = _twilioSettingCacheService.GetTwilioSettingCache().FirstOrDefault();
+            var callerId = twilioSetting.CallId;
+            var twiml = new VoiceResponse();
+
+            // someone calls into my Twilio Number, there is no thisDeviceIdentity passed to the /voice endpoint 
+            if (string.IsNullOrEmpty(callingDeviceIdentity))
+            {
+                var calledagent = _services.ReadMany<Agent>(x => x.State == 1).FirstOrDefault();
+                var avaliableIndentity = calledagent?.DeviceNumber;
+                if (avaliableIndentity == null)
+                {
+                    var exceptionResult = new VoiceResponse();
+                    exceptionResult.Say("Sorry, there are no valiable agents now.");
+                    return Content(exceptionResult.ToString(), "text/xml");
+                }
+                var dial = new Dial();
+                var client = new Client();
+                client.Identity(avaliableIndentity);
+                dial.Append(client);
+                twiml.Append(dial);
+                calledagent.State = 2;
+                await _services.UpdateAndSaveAsync<Agent>(calledagent);
+            }
+            // Call someone
+            else
+            {
+                var callingAgent = await _services.ReadSingleAsync<Agent>(x => x.DeviceNumber == callingDeviceIdentity);
+                callingAgent.State = 2;
+                await _services.UpdateAndSaveAsync<Agent>(callingAgent);
+                var dial = new Dial(callerId: callerId);
+
+                // check if the 'To' property in the POST request is
+                // a client name or a phone number
+                // and dial appropriately using either Number or Client
+                if (Regex.IsMatch(to, "^[\\d\\+\\-\\(\\) ]+$"))
+                {
+                    Console.WriteLine("Match is true");
+                    dial.Number(to);
+                }
+                else
+                {
+                    if (to.StartsWith("EXT:"))
+                    {
+                        var client = new Client();
+                        client.Identity(to.Substring(4));
+                        dial.Append(client);
+                    }
+                    else
+                    {
+                        var exceptionResult = new VoiceResponse();
+                        exceptionResult.Say("Sorry, the operation don't support.");
+                        return Content(exceptionResult.ToString(), "text/xml");
+                    }
+
+                }
+                twiml.Append(dial);
+            }
+            Console.WriteLine(twiml.ToString());
+            _logger.LogInformation($"-----voiceResult: {twiml.ToString()}");
+            return Content(twiml.ToString(), "text/xml");
+        }
+    }
+}
